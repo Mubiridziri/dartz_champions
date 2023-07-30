4566fa9606efede62568f47715cf2036a4083fd0
Yury Potapov <yupotapov@icloud.com>
2023-07-30 23:08:35 +0400

    Initial Commit

---
+++ DartZ Champions/DartZ_ChampionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DartZ_ChampionsApp.swift
+//  DartZ Champions
+//
+//  Created by Юрий Потапов on 30.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct DartZ_ChampionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
